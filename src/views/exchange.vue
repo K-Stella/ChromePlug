ea0b2cd8ca89fb48e8317d0f4fe7be34eeaef904
XuZhong.Chen <q20020628@163.com>
2018-10-26 16:54:16 +0800

    简单静态页面.

--- src/views/exchange.vue
+++ src/views/exchange.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="app_container">
     我是汇率换算页面
+    <div
+      class="currency_list"
+      v-for="(item,index) in (currency)"
+      :key="index"
+    >
+      <el-card
+        class="box-card"
+        shadow="hover"
+      >
+        <div class="item_left">
+          <img :src=item.src /><span @click="showlist(index)">{{ item.countryName }}</span>
+          <select v-model="item.index" name="countryOptions" @change="changeCountry(item,countryOptions)" >
+            <option :value="options.name" v-for="(options) in countryOptions" :key="options">
+              <span>{{ options.name }}</span>
+            </option>
+          </select>
+        </div>
+        <div class="item_right" style="float: right; display: grid; margin-right: 50px;">
+          <el-input v-model="input" placeholder="100"></el-input>
+          <span class="money_name" style="text-align: right;">{{ item.moneyName }}</span>
+        </div>
+      </el-card>
+    </div>
     <!-- <div v-for="item in currency" :key="item">
       <el-card class="box-card" shadow="hover">
         <div class="text item" style="height: 25px; margin-bottom: 5px;">
           <div class="item_left" style="float: left;">
-            <img class="countryFlag" :src=item.src style="width: 50px;" />
-            <span class="countryName">{{ item.countryName }}</span>
+            <span><img class="countryFlag" :src=item.src style="width: 50px;" />{{ item.countryName }}</span>
+              <ul v-show="flag">
+                <li @click="checkThis(item.countryName,item.src)" v-for="item in currency" :key="item">
+                  <img :src="item.src" />
+                  <span>{{ item.countryName }}</span>
+                </li>
+              </ul>
           </div>
           <div class="item_right" style="float: right; display: grid;">
             <el-input v-model="input" placeholder="100"></el-input>
@@ -23,6 +51,8 @@ export default {
   name: 'Exchange',
   data () {
     return {
+      selected: '',
+      flag: false,
       currency: [
         {
           src: '../../static/img/china.png',
@@ -31,30 +61,78 @@ export default {
           moneyName: '人民币¥'
         },
         {
-          src: '../../static/img/china.png',
-          countryName: '中国',
+          src: '../../static/img/america.png',
+          countryName: '美国',
           money: '100',
-          moneyName: '人民币¥'
+          moneyName: '美元¥'
         },
         {
-          src: '../../static/img/china.png',
-          countryName: '中国',
+          src: '../../static/img/italy.png',
+          countryName: '意大利',
           money: '100',
-          moneyName: '人民币¥'
+          moneyName: '意大利元¥'
         },
         {
-          src: '../../static/img/china.png',
-          countryName: '中国',
+          src: '../../static/img/hongkong.png',
+          countryName: '香港',
           money: '100',
-          moneyName: '人民币¥'
+          moneyName: '港币¥'
         },
         {
-          src: '../../static/img/china.png',
-          countryName: '中国',
+          src: '../../static/img/macao.png',
+          countryName: '澳门',
           money: '100',
-          moneyName: '人民币¥'
+          moneyName: '澳元币¥'
+        }
+      ],
+      countryOptions: [
+        {
+          value: '0',
+          name: '英国',
+          moneyName: '英镑¥',
+          src: '../../static/img/china.png'
+        },
+        {
+          value: '1',
+          name: '美国',
+          moneyName: '美元¥',
+          src: '../../static/img/america.png'
+        },
+        {
+          value: '2',
+          name: '澳大利',
+          moneyName: '意大利元¥',
+          src: '../../static/img/china.png'
+        },
+        {
+          value: '3',
+          name: '香港',
+          moneyName: '港币¥',
+          src: '../../static/img/hongkong.png'
+        },
+        {
+          value: '4',
+          name: '中国',
+          moneyName: '人民币¥',
+          src: '../../static/img/china.png'
         }
       ]
+    }
+  },
+  methods: {
+    showlist: function (index) {
+      console.log(index)
+      this.flag = !this.flag
+      console.log('111' + this.countryOptions[index].flag)
+    },
+    changeCountry: function (item, countryOptions) {
+      item.countryName = item.index
+      for (let i = 0; i < countryOptions.length; i++) {
+        if (countryOptions[i].name === item.index) {
+          item.src = countryOptions[i].src
+          item.moneyName = countryOptions[i].moneyName
+        }
+      }
     }
   }
 }
@@ -65,7 +143,20 @@ export default {
     padding: 0;
     margin: 0;
   }
+  .item_left {
+    float: left;
+  }
+  .item_left img{
+    width: 50px;
+  }
+  .item_left span{
+    margin-bottom: 5px;
+  }
   .el-card {
+    width: 850px;
+    margin: 0 auto;
+  }
+  /* .el-card {
       border: 1px solid #464646;
       background-color: #434958eb !important;
       color: #26282b;
@@ -99,5 +190,5 @@ export default {
     -webkit-transition: border-color .2s cubic-bezier(.645,.045,.355,1);
     transition: border-color .2s cubic-bezier(.645,.045,.355,1);
     width: 100%;
-  }
+  } */
 </style>
